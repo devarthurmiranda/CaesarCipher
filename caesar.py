f70b366ddab8737b8313826671ccf0c7b7d35ffd
Arthur Miranda <contato.arthurmorais.dev@gmail.com>
2024-10-03 11:56:46 -0300

    implementing modular vs not modular

--- caesar.py
+++ caesar.py
@@ -1,7 +1,9 @@
 # Unicode link - https://en.wikipedia.org/wiki/List_of_Unicode_characters
 # All letters start at 65
 
-def encrypt(text, shift):
+import time
+
+def encrypt_modular(text, shift):
     result = ""
     for l in range(len(text)):
         char = text[l]
@@ -21,7 +23,7 @@ def encrypt(text, shift):
 
     return result
 
-def decrypt(text, shift):
+def decrypt_modular(text, shift):
     result = ""
     # Traverse each character in the text
     for i in range(len(text)):
@@ -39,8 +41,86 @@ def decrypt(text, shift):
 
     return result
 
+def encrypt(text, shift):
+    result = ""
+
+    # Traverse each character in the text
+    for i in range(len(text)):
+        char = text[i]
+
+        # Encrypt uppercase letters
+        if char.isupper():
+            # Shift character
+            shifted = ord(char) + shift
+            # Check if the shifted character goes beyond 'Z'
+            if shifted > ord('Z'):
+                # Wrap around to the beginning of the alphabet
+                shifted -= 26
+            result += chr(shifted)
+
+        # Encrypt lowercase letters
+        elif char.islower():
+            # Shift character
+            shifted = ord(char) + shift
+            # Check if the shifted character goes beyond 'z'
+            if shifted > ord('z'):
+                # Wrap around to the beginning of the alphabet
+                shifted -= 26
+            result += chr(shifted)
+
+        else:
+            # Non-alphabet characters remain unchanged
+            result += char
+
+    return result
+
+
+def decrypt(text, shift):
+    result = ""
+
+    # Traverse each character in the text
+    for i in range(len(text)):
+        char = text[i]
+
+        # Decrypt uppercase letters
+        if char.isupper():
+            # Shift character
+            shifted = ord(char) - shift
+            # Check if the shifted character is before 'A'
+            if shifted < ord('A'):
+                # Wrap around to the end of the alphabet
+                shifted += 26
+            result += chr(shifted)
+
+        # Decrypt lowercase letters
+        elif char.islower():
+            # Shift character
+            shifted = ord(char) - shift
+            # Check if the shifted character is before 'a'
+            if shifted < ord('a'):
+                # Wrap around to the end of the alphabet
+                shifted += 26
+            result += chr(shifted)
+
+        else:
+            # Non-alphabet characters remain unchanged
+            result += char
+
+    return result
+
 # Test
-text = "This is some confidential Roman empire information writen by Julius Caesar! Fighting!"
+st1 = time.time()
+text = "This is some confidential Roman empire information writen by Julius Caesar! Fighting! Nulla quis leo blandit, maximus libero sed, imperdiet felis. Maecenas non euismod elit, et mattis lorem. Sed consequat, nibh id dapibus porta, dolor velit scelerisque eros, eu tincidunt felis sem sed erat. Fusce eget ante libero. Suspendisse viverra erat sit amet metus pharetra, vitae cursus tellus porttitor. Curabitur vitae hendrerit est, ac fringilla odio. Duis mollis fringilla enim. Fusce consectetur scelerisque ex, et eleifend libero fringilla id. In magna tortor, sagittis vel commodo gravida, tempor id libero. Cras id condimentum dui."
+encrypted_text = encrypt_modular(text, 3)
+print(f"Encrypted text: {encrypted_text}")
+print(f"Decrypted text {decrypt_modular(encrypted_text, 3)}")
+et1 = time.time()
+print(f"Elapsed time (modular): {et1 - st1}")
+
+st2 = time.time()
+text = "This is some confidential Roman empire information writen by Julius Caesar! Fighting! Nulla quis leo blandit, maximus libero sed, imperdiet felis. Maecenas non euismod elit, et mattis lorem. Sed consequat, nibh id dapibus porta, dolor velit scelerisque eros, eu tincidunt felis sem sed erat. Fusce eget ante libero. Suspendisse viverra erat sit amet metus pharetra, vitae cursus tellus porttitor. Curabitur vitae hendrerit est, ac fringilla odio. Duis mollis fringilla enim. Fusce consectetur scelerisque ex, et eleifend libero fringilla id. In magna tortor, sagittis vel commodo gravida, tempor id libero. Cras id condimentum dui."
 encrypted_text = encrypt(text, 3)
 print(f"Encrypted text: {encrypted_text}")
 print(f"Decrypted text {decrypt(encrypted_text, 3)}")
+et2 = time.time()
+print(f"Elapsed time (not modular): {et2 - st2}")
